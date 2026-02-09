cdc9438ab7253045eb50dd923b93d6bab6514e74
rdevine16 <ryan.devine16@gmail.com>
2026-02-09 00:22:27 +0000

    Move analytics nav to horizontal tabs

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+// This is the layout for all admin pages. It is used to wrap the admin pages and provide a common layout for them.
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
