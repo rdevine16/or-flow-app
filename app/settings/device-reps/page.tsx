c06058d2181211de45390524e6286cfe29ce12d1
Ryan Devine <ryan.devine16@gmail.com>
2026-02-10 20:59:21 -0500

    d

--- app/settings/device-reps/page.tsx
+++ app/settings/device-reps/page.tsx
@@ -584,7 +584,7 @@ export default function DeviceRepsPage() {
           </Modal>
 
           {/* Success Modal */}
-          <Modal open={inviteLinkModal.isOpen} onClose={() => setInviteLinkModal({ isOpen: false, link: '', email: '' })} size="sm">
+<Modal open={inviteLinkModal.isOpen} onClose={() => setInviteLinkModal({ isOpen: false, link: '', email: '' })} title="Invite Sent!" size="sm">
             <div className="text-center py-4">
               <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mx-auto mb-4">
                 <Check className="w-8 h-8 text-green-600" />
